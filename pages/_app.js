55107e6c8952db36be7876b6fa31ddf95a9838f0
Chester Dilday <chester@keystoneendurance.com>
2025-12-24 16:35:03 -0500

    Add mobile meta tags and analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
+import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes" />
+        <meta charSet="utf-8" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <title>Holiday Nutrition Planner - Keystone Endurance</title>
+        <meta name="description" content="Plan your holiday nutrition strategy. Calculate personalized macros for training and rest days during the holiday season." />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
     </>
-  )
+  );
 }
+
+export default MyApp;
